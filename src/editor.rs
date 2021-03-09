3c0f7d5554dd866e610220344bbfc0bba3b3b78a
Waldemar <waldemar.schwan@uswitch.com>
2021-03-09 19:05:54 +0000

    clean exit

--- src/editor.rs
+++ src/editor.rs
@@ -5,23 +5,25 @@ use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
     pub fn default() -> Self {
-        Self {}
+        Self { should_quit: false }
     }
 
-    fn process_keypress(&self) -> Result<(), std::io::Error> {
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
         let pressed_key = read_key()?;
         match pressed_key {
-            Key::Ctrl('q') => panic!("Program end"),
+            Key::Ctrl('q') => self.should_quit = true,
             _ => (),
         }
         Ok(())
     }
 
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         // There are a few things to note here. First, we are using termion to provide stdout, the counterpart of stdin from above with a function called into_raw_mode(), which we are calling. But why are we calling that method on stdout to change how we read from stdin? The answer is that terminals have their states controlled by the writer, not the reader. The writer is used to draw on the screen or move the cursor, so it is also used to change the mode as well.
         // RAW mode
         // 1. no line buffering
@@ -31,6 +33,9 @@ impl Editor {
         loop {
             if let Err(error) = self.process_keypress() {
                 die(error);
+            }
+            if self.should_quit {
+                break;
             }
         }
     }
@@ -43,7 +48,6 @@ fn read_key() -> Result<Key, std::io::Error> {
         if let Some(key) = io::stdin().lock().keys().next() {
             return key;
         }
-        // TODO: Finish chapter 3 -> https://www.philippflenker.com/hecto-chapter-3/
         // if None we loop ie busy wait for actual input
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 // Based on https://www.philippflenker.com/hecto-chapter-2/
 
 mod editor;
-
 use editor::Editor;
 
 fn main() {
-    let editor = Editor::default();
-    editor.run();
+    Editor::default().run();
 }
